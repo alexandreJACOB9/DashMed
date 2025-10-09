0e54472bc20a995701e8d1d94e2dad3ddd0bfa8a
Théo GHEUX <96632622+TheoGHEUX@users.noreply.github.com>
2025-10-09 12:01:49 +0200

    Update profile.php

--- SITE/Views/profile.php
+++ SITE/Views/profile.php
@@ -15,6 +15,7 @@ $last  = $parts[1] ?? '';
     <meta charset="utf-8">
     <title>Profil - DashMed</title>
     <meta name="viewport" content="width=device-width,initial-scale=1">
+    <meta name="description" content="Consultez votre profil DashMed !">
     <link rel="preconnect" href="https://fonts.googleapis.com">
     <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
     <link href="https://fonts.googleapis.com/css2?family=Poppins:wght@400;500;600&display=swap" rel="stylesheet">
